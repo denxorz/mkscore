8ff1337191e1db060e16ec850cefff2eabefe5b3
Dennis Geldhof <dennis@geldhof.eu>
2025-02-28 23:58:18 +0100

    Change to fab button

--- src/mkscoreapp/src/pages/index.vue
+++ src/mkscoreapp/src/pages/index.vue
@@ -1,7 +1,12 @@
 <template>
-  <HelloWorld />
+  <v-dialog v-model="dialog" max-width="500">
+    <HelloWorld />
+  </v-dialog>
+
+  <v-fab class="ms-4" icon="mdi-plus"
+    @click="dialog = !dialog"></v-fab>
 </template>
 
 <script lang="ts" setup>
-  //
+const dialog = ref(false)
 </script>
